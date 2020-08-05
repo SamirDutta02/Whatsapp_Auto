1dfc6886720d80cf704a0e0b37b3eec32814af09
Samir Dutta <samirdutta2399@gmail.com.com>
2020-08-06 00:45:31 +0530

    Marketing

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-var='n0'
-def fun():
-    global var
-    var = "hello"
-    var2= "lol"
-    return var2
-print(var)
-fun()
-print(var)
-print(fun())
+import pandas as pd
+print(pd.read_csv('FL_insurance_sample', keep_default_na=False))
